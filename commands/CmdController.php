7d6ef21e96b86062f14f8aca10a611d8059dc8e8
hbhe <hbhe@wosotech.com>
2014-07-24 15:21:54 +0800

    aa
    Signed-off-by: hbhe <hbhe@wosotech.com>

--- commands/CmdController.php
+++ commands/CmdController.php
@@ -142,8 +142,8 @@ class CmdController extends Controller
 					//new \app\models\ButtonView('lucynew', 'http://www.hoyatech.net/wx/webtest/lucyNew.php'),
 					new \app\models\ButtonView('2048', 'http://www.hoyatech.net/wx/webtest/2048/index.htm'),
 				]),
-				//new \app\models\ButtonView('★促销商品', Yii::$app->wx->WxGetOauth2Url('snsapi_base', 'wap/prom:'.Yii::$app->wx->getGhid())),
-				new \app\models\ButtonView('★促销商品', 'http://www.hoyatech.net/wx/webtest/index.php?r=wap/prom&gh_id=gh_1ad98f5481f3'),
+				new \app\models\ButtonView('促销商品', Yii::$app->wx->WxGetOauth2Url('snsapi_base', 'wap/prom:'.Yii::$app->wx->getGhid())),
+				//new \app\models\ButtonView('促销商品', 'http://www.hoyatech.net/wx/webtest/index.php?r=wap/prom&gh_id=gh_1ad98f5481f3'),
 				new \app\models\ButtonComplex('我的服务', [
 					//new \app\models\ButtonClick('个性化账单', 'FuncQueryAccount'),
 					new \app\models\ButtonClick('本地生活', 'FuncQueryFee'),
