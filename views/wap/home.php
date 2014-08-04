cb7ba0d9dd2075aaebfe4ac8cea964fbf95f5eea
kzeng <kzeng@wosotech.com>
2014-08-05 01:27:16 +0800

    mod
    Signed-off-by: kzeng <kzeng@wosotech.com>

--- views/wap/home.php
+++ views/wap/home.php
@@ -1,4 +1,13 @@
-﻿<!DOCTYPE html>
+﻿<?php
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\widgets\Breadcrumbs;
+use app\assets\JqmAsset;
+JqmAsset::register($this);
+
+?>
+
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
@@ -20,7 +29,7 @@ style="margin:10px;"
 <div class="row">
 <div class="col-sm-6 col-md-3">
     <div class="thumbnail tile tile-wide tile-orange">
-        <a href="#" >
+        <a href="http://mp.weixin.qq.com/s?__biz=MzA4ODkwOTYxMA==&mid=203635121&idx=1&sn=b47bc2f6a4490819227853e8ffed72c9#rd" >
             <h1>八月浪漫季
             </h1>
         </a>
@@ -29,7 +38,7 @@ style="margin:10px;"
 
 <div class="col-sm-6 col-md-3">
     <div class="thumbnail tile tile-medium tile-blue col-md-3" >
-        <a href="#" >
+        <a href="<?php echo Url::to(['wap/product'], true) ; ?>" >
             <h1>自由组合套餐
             </h1>
         </a>
@@ -38,7 +47,7 @@ style="margin:10px;"
 
 <div class="col-sm-6 col-md-3">
     <div class="thumbnail tile tile-medium tile-red col-md-3">
-        <a href="#" >
+        <a href="<?php echo Url::to(['wap/cardwo'], true) ; ?>" >
             <h1>微信沃卡
             </h1>
         </a>
@@ -47,7 +56,7 @@ style="margin:10px;"
 
 <div class="col-sm-6 col-md-3">
     <div class="thumbnail tile tile-medium tile-lime col-md-3">
-        <a href="#" >
+        <a href="<?php echo Url::to(['wap/g2048'], true) ; ?>" >
             <h1>游戏2048
             </h1>
         </a>
@@ -56,7 +65,7 @@ style="margin:10px;"
 
 <div class="col-sm-6 col-md-3">
     <div class="thumbnail tile tile-medium tile-yellow col-md-3">
-        <a href="#" >
+        <a href="<?php echo Url::to(['wap/cardxiaoyuan'], true) ; ?>" >
             <h1>沃派校园套餐
             </h1>
         </a>
@@ -65,7 +74,7 @@ style="margin:10px;"
 
 <div class="col-sm-6 col-md-3">
     <div class="thumbnail tile tile-medium tile-pink col-md-3">
-        <a href="#" >
+        <a href="http://m.10010.com/mall-mobile/NumList/search" >
             <h1>精选靓号
             </h1>
         </a>
@@ -74,7 +83,7 @@ style="margin:10px;"
 
 <div class="col-sm-6 col-md-3">
     <div class="thumbnail tile tile-wide tile-green">
-        <a href="#" >
+        <a href="<?php echo Url::to(['wap/mobilelist'], true) ; ?>" >
             <h1>特惠手机
             </h1>
         </a>
@@ -83,23 +92,25 @@ style="margin:10px;"
 
 <div class="col-sm-6 col-md-3">
     <div class="thumbnail tile tile-medium tile-magenta col-md-3">
-        <a href="#" >
+        <a href="http://mp.weixin.qq.com/s?__biz=MzA4ODkwOTYxMA==&mid=203635121&idx=1&sn=b47bc2f6a4490819227853e8ffed72c9#rd" >
             <h1>关注有礼
             </h1>
         </a>
     </div>
 </div>
+
 <div class="col-sm-6 col-md-3">
     <div class="thumbnail tile tile-medium tile-teal col-md-3">
-        <a href="#" >
+        <a href="<?php echo Url::to(['wap/suggest'], true) ; ?>" >
             <h1>用户吐槽
             </h1>
         </a>
     </div>
 </div>
+
 <div class="col-sm-6 col-md-3">
     <div class="thumbnail tile tile-medium tile-turquoise col-md-3">
-        <a href="#" >
+        <a href="http://mp.weixin.qq.com/s?__biz=MzA4ODkwOTYxMA==&mid=203609285&idx=1&sn=06c623779131934da8368482a55e5ba1#rd" >
             <h1>流量包订购
             </h1>
         </a>
@@ -107,7 +118,7 @@ style="margin:10px;"
 </div>
 <div class="col-sm-6 col-md-3">
     <div class="thumbnail tile tile-medium tile-green-sea col-md-3">
-        <a href="#" >
+        <a href="http://wap.10010.com/t/siteMap.htm?menuId=query" >
             <h1>账单查询
             </h1>
         </a>
