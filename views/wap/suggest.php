279093d75bbc8ec1b45b1dfdfecede860c89d899
kzeng <kzeng@wosotech.com>
2014-08-06 23:58:02 +0800

    mod
    Signed-off-by: kzeng <kzeng@wosotech.com>

--- views/wap/suggest.php
+++ views/wap/suggest.php
@@ -37,7 +37,7 @@ Yii::$app->wx->setGhid($gh_id);
     </div>
 
     <div data-role="content">
-        <?php $form = ActiveForm::begin(['id' => 'contact-form', 'class'=>'ui-field-contain']); ?>
+        <?php $form = ActiveForm::begin(['id' => 'contact-form','class'=>'ui-field-contain']); ?>
 
              <?= $form->field($ar, 'detail')->textarea(['maxlength' => '256', 'placeholder'=>'我要吐槽的详细内容'])->label(false); ?>
 
@@ -99,51 +99,8 @@ Yii::$app->wx->setGhid($gh_id);
     </div>
 </div> <!-- page1 end -->
 
-<div data-role="page" id="page2" data-theme="e">
-    <div data-role="header" data-add-back-btn="true" data-back-btn-text="返回">
-        <h1>襄阳联通官方微信营业厅</h1>
-    </div>
-
-    <div data-role="content">
-        <h3>感谢您的吐槽！</h3>
-    </div>
-
-    <div data-role="footer">
-        <h4>&copy; 襄阳联通 2014</h4>
-    </div>
-</div> <!-- page2 end -->
 
 <?php $this->endBody() ?>
 </body>
 </html>
 <?php $this->endPage() ?>
-
-
-<script>
-    $(document).on("pageshow", "#page1", function(){
-        //alert('aaa');
-
-        //submit form
-        /*
-        $('#submitBtn').click(function(){
-
-            $.mobile.changePage("#page2",{transition:"slide"});
-            return false;
-        });
-        */
-
-           /*
-        $( "#contact-form" ).submit(function( event ) {
-           //alert( "Handler for .submit() called." );
-           //event.preventDefault();
-            $.mobile.changePage("#page2",{transition:"slide"});
-           // return false;
-
-        });
-        */
-
-    });
-</script>
-
-
-
