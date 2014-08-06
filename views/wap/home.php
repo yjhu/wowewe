30f1ce87355438913b1562225feffb6c5e578425
kzeng <kzeng@wosotech.com>
2014-08-06 16:47:33 +0800

    mod
    Signed-off-by: kzeng <kzeng@wosotech.com>

--- views/wap/home.php
+++ views/wap/home.php
@@ -46,150 +46,150 @@ Yii::$app->wx->setGhid($gh_id);
 
     <div data-role="content">
 
-    <div class="wrap">
-    <div class="container" style="padding-top:0px;">
+        <div class="wrap">
+            <div class="container" style="padding-top:0px;">
 
-        <!--
-        每个小色块换成 345x190（原230x137）
-        长色块换成 700x190（原来是 465x127）
-        整个布局换成 2 - 2 - 1 - 2 - 1 - 2
-        第一行： 自由组合套餐， 微信沃卡
-        第二行： 沃派校园套餐， 精选靓号
-        第三行：特惠手机
-        第四行：账单查询，流量包订购
-        第5行：关注有礼
-        第6行：用户吐槽，2048
-        另，画内色块间距，横竖都换成10（原来是5）
-        -->
+                <!--
+                每个小色块换成 345x190（原230x137）
+                长色块换成 700x190（原来是 465x127）
+                整个布局换成 2 - 2 - 1 - 2 - 1 - 2
+                第一行： 自由组合套餐， 微信沃卡
+                第二行： 沃派校园套餐， 精选靓号
+                第三行：特惠手机
+                第四行：账单查询，流量包订购
+                第5行：关注有礼
+                第6行：用户吐槽，2048
+                另，画内色块间距，横竖都换成10（原来是5）
+                -->
 
-    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-            <!--
-            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-            -->
-        </ol>
+                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                        <!--
+                        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                        -->
+                    </ol>
 
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner" role="listbox">
-            <div class="item active">
-                <img src="../web/images/metro_home_head1.jpg" alt="八月浪漫季">
-                <div class="carousel-caption">
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                            <img src="../web/images/metro_home_head1.jpg" alt="八月浪漫季">
+                            <div class="carousel-caption">
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <img src="../web/images/metro_home_head2.jpg" alt="八月浪漫季">
+                            <div class="carousel-caption">
+                            </div>
+                        </div>
+                        <!--
+                        <div class="item">
+                            <img src="../web/images/metro_home_head1.jpg" alt="八月浪漫季">
+                            <div class="carousel-caption">
+                            </div>
+                        </div>
+                        -->
+                    </div>
+
+                    <!-- Controls -->
+                    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
                 </div>
-            </div>
 
-            <div class="item">
-                <img src="../web/images/metro_home_head2.jpg" alt="八月浪漫季">
-                <div class="carousel-caption">
+
+                <div class="row" style="margin-top: 10px">
+                    <div class="col-md-6 col-xs-6" >
+                        <a href="<?php echo Url::to(['wap/product'], true) ; ?>" >
+                            <img width=100% src="../web/images/metro-zyzhtc.jpg">
+                        </a>
+                    </div>
+                    <div class="col-md-6 col-xs-6" >
+                        <a href="<?php echo Url::to(['wap/cardwo'], true) ; ?>" >
+                            <img width=100% src="../web/images/metro-wxwk.jpg">
+                        </a>
+                    </div>
                 </div>
-            </div>
-            <!--
-            <div class="item">
-                <img src="../web/images/metro_home_head1.jpg" alt="八月浪漫季">
-                <div class="carousel-caption">
+
+                <div class="row">
+                    <div class="col-md-6 col-xs-6" style="margin: 10px auto">
+                        <a href="<?php echo Url::to(['wap/cardxiaoyuan'], true) ; ?>" >
+                            <img width=100% src="../web/images/metro-wpxytc.jpg">
+                        </a>
+                    </div>
+                    <div class="col-md-6 col-xs-6" style="margin: 10px auto">
+                        <a href="http://m.10010.com/mall-mobile/NumList/search" >
+                            <img width=100% src="../web/images/metro-jxlh.jpg">
+                        </a>
+                    </div>
                 </div>
+
+                <div class="row" >
+                    <div class="col-md-12 col-xs-12" >
+                        <a href="<?php echo Url::to(['wap/mobilelist'], true) ; ?>" >
+                            <img width=100% src="../web/images/metro-thsj.jpg">
+                        </a>
+                    </div>
+                </div>
+
+                <div class="row" >
+                    <div class="col-md-6 col-xs-6" style="margin: 10px auto">
+                        <a href="http://wap.10010.com/t/siteMap.htm?menuId=query" >
+                            <img width=100% src="../web/images/metro-zdcx.jpg">
+                        </a>
+                    </div>
+                    <div class="col-md-6 col-xs-6" style="margin: 10px auto">
+                        <a href="http://mp.weixin.qq.com/s?__biz=MzA4ODkwOTYxMA==&mid=203609285&idx=1&sn=06c623779131934da8368482a55e5ba1#rd" >
+                            <img width=100% src="../web/images/metro-llbdg.jpg">
+                        </a>
+                    </div>
+                </div>
+
+                <div class="row" >
+                    <div class="col-md-12 col-xs-12">
+                        <a href="http://mp.weixin.qq.com/s?__biz=MzA4ODkwOTYxMA==&mid=203635121&idx=1&sn=b47bc2f6a4490819227853e8ffed72c9#rd" >
+                            <img width=100% src="../web/images/metro-gzyl.jpg">
+                        </a>
+                    </div>
+                </div>
+
+                <div class="row" >
+                    <div class="col-md-6 col-xs-6" style="margin: 10px auto">
+                        <a href="<?php echo Url::to(['wap/suggest'], true) ; ?>" >
+                            <img width=100% src="../web/images/metro-yhtc.jpg">
+                        </a>
+                    </div>
+                    <div class="col-md-6 col-xs-6" style="margin: 10px auto">
+                        <a href="<?php echo Url::to(['wap/g2048'], true) ; ?>" >
+                            <img width=100% src="../web/images/metro-2048.jpg">
+                        </a>
+                    </div>
+                </div>
+
+                <div class="row" >
+                    <div class="col-md-12 col-xs-12">
+                        <!--
+                        <img width=100% src="../web/images/metro-gzyl.jpg">
+                        -->
+                        <img class="img-responsive" src="/wx/web/images/wx-tuiguang2.jpg" alt="">
+                    </div>
+                </div>
+
             </div>
-            -->
         </div>
-
-        <!-- Controls -->
-        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right"></span>
-            <span class="sr-only">Next</span>
-        </a>
     </div>
 
-
-<div class="row" style="margin-top: 10px">
-    <div class="col-md-6 col-xs-6" >
-        <a href="<?php echo Url::to(['wap/product'], true) ; ?>" >
-        <img width=100% src="../web/images/metro-zyzhtc.jpg">
-        </a>
+    <div data-role="footer">
+        <h4>&copy; 襄阳联通 2014</h4>
     </div>
-    <div class="col-md-6 col-xs-6" >
-        <a href="<?php echo Url::to(['wap/cardwo'], true) ; ?>" >
-        <img width=100% src="../web/images/metro-wxwk.jpg">
-        </a>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-md-6 col-xs-6" style="margin: 10px auto">
-        <a href="<?php echo Url::to(['wap/cardxiaoyuan'], true) ; ?>" >
-        <img width=100% src="../web/images/metro-wpxytc.jpg">
-        </a>
-    </div>
-    <div class="col-md-6 col-xs-6" style="margin: 10px auto">
-        <a href="http://m.10010.com/mall-mobile/NumList/search" >
-        <img width=100% src="../web/images/metro-jxlh.jpg">
-        </a>
-    </div>
-</div>
-
-<div class="row" >
-    <div class="col-md-12 col-xs-12" >
-        <a href="<?php echo Url::to(['wap/mobilelist'], true) ; ?>" >
-        <img width=100% src="../web/images/metro-thsj.jpg">
-        </a>
-    </div>
-</div>
-
-<div class="row" >
-    <div class="col-md-6 col-xs-6" style="margin: 10px auto">
-        <a href="http://wap.10010.com/t/siteMap.htm?menuId=query" >
-        <img width=100% src="../web/images/metro-zdcx.jpg">
-        </a>
-    </div>
-    <div class="col-md-6 col-xs-6" style="margin: 10px auto">
-        <a href="http://mp.weixin.qq.com/s?__biz=MzA4ODkwOTYxMA==&mid=203609285&idx=1&sn=06c623779131934da8368482a55e5ba1#rd" >
-        <img width=100% src="../web/images/metro-llbdg.jpg">
-        </a>
-    </div>
-</div>
-
-<div class="row" >
-    <div class="col-md-12 col-xs-12">
-        <a href="http://mp.weixin.qq.com/s?__biz=MzA4ODkwOTYxMA==&mid=203635121&idx=1&sn=b47bc2f6a4490819227853e8ffed72c9#rd" >
-        <img width=100% src="../web/images/metro-gzyl.jpg">
-        </a>
-    </div>
-</div>
-
-<div class="row" >
-    <div class="col-md-6 col-xs-6" style="margin: 10px auto">
-        <a href="<?php echo Url::to(['wap/suggest'], true) ; ?>" >
-        <img width=100% src="../web/images/metro-yhtc.jpg">
-        </a>
-    </div>
-    <div class="col-md-6 col-xs-6" style="margin: 10px auto">
-        <a href="<?php echo Url::to(['wap/g2048'], true) ; ?>" >
-        <img width=100% src="../web/images/metro-2048.jpg">
-        </a>
-    </div>
-</div>
-
-<div class="row" >
-    <div class="col-md-12 col-xs-12">
-            <!--
-            <img width=100% src="../web/images/metro-gzyl.jpg">
-            -->
-            <img class="img-responsive" src="/wx/web/images/wx-tuiguang2.jpg" alt="">
-    </div>
-</div>
-
-</div>
-</div>
-</div>
-
-<div data-role="footer">
-    <h4>&copy; 襄阳联通 2014</h4>
-</div>
 
 </div> <!-- page1 end -->
 <?php $this->endBody() ?>
--- views/wap/suggest.php
+++ views/wap/suggest.php
@@ -1,8 +1,9 @@
 ﻿<?php
 use yii\helpers\Html;
-use yii\bootstrap\ActiveForm;
+//use yii\bootstrap\ActiveForm;
+use yii\widgets\ActiveForm;
 use yii\helpers\Url;
-use yii\bootstrap\Alert;
+//use yii\bootstrap\Alert;
 use app\models\U;
 
 use app\assets\JqmAsset;
@@ -29,25 +30,72 @@ Yii::$app->wx->setGhid($gh_id);
 <body>
 <?php $this->beginBody() ?>
 <div data-role="page" id="page1" data-theme="e">
-
     <div data-role="header" data-theme="e">
         <h1>襄阳联通官方微信营业厅</h1>
     </div>
 
     <div data-role="content">
+        <?php $form = ActiveForm::begin(['id' => 'contact-form', 'class'=>'ui-field-contain']); ?>
 
+             <?= $form->field($ar, 'detail')->textarea(['maxlength' => '256', 'placeholder'=>'我要吐槽的详细内容'])->label(false); ?>
+
+            <?= Html::submitButton('我要吐槽', ['class' => 'ui-shadow ui-btn ui-corner-all', 'name' => 'contact-button']) ?>
+
+        <?php ActiveForm::end(); ?>
+            <br><br>
+        <?php echo Html::img(Url::to('images/wx-tuiguang2.jpg'), ['class'=>'img-responsive','width'=>'100%']); ?>
     </div>
 
     <div data-role="footer">
         <h4>&copy; 襄阳联通 2014</h4>
     </div>
-
 </div> <!-- page1 end -->
+
+<div data-role="page" id="page2" data-theme="e">
+    <div data-role="header" data-add-back-btn="true" data-back-btn-text="返回">
+        <h1>襄阳联通官方微信营业厅</h1>
+    </div>
+
+    <div data-role="content">
+        <h3>感谢您的吐槽！</h3>
+    </div>
+
+    <div data-role="footer">
+        <h4>&copy; 襄阳联通 2014</h4>
+    </div>
+</div> <!-- page2 end -->
+
 <?php $this->endBody() ?>
 </body>
 </html>
 <?php $this->endPage() ?>
 
+
+<script>
+    $(document).on("pageshow", "#page1", function(){
+        //alert('aaa');
+
+        //submit form
+        /*
+        $('#submitBtn').click(function(){
+
+            $.mobile.changePage("#page2",{transition:"slide"});
+            return false;
+        });
+        */
+
+           /*
+        $( "#contact-form" ).submit(function( event ) {
+           //alert( "Handler for .submit() called." );
+           //event.preventDefault();
+            $.mobile.changePage("#page2",{transition:"slide"});
+           // return false;
+
+        });
+        */
+
+    });
+</script>
 
 
 
