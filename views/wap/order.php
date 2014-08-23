b72c22cfdb61d63cbc7fac9640fd8312d12ad027
kzeng <kzeng@wosotech.com>
2014-08-23 08:00:00 +0800

    mod
    Signed-off-by: kzeng <kzeng@wosotech.com>

--- views/wap/order.php
+++ views/wap/order.php
@@ -29,12 +29,6 @@ $basename = basename(__FILE__, '.php');
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
 
-	<style type="text/CSS">
-		.ui-content {
-			padding: 0.5em !important;
-		}
-	</style>
-
 	<?php $this->head() ?>
 </head>
 
@@ -62,7 +56,6 @@ $basename = basename(__FILE__, '.php');
 	</div>
 </div>
 
-
 <div data-role="page" id="orderdetail" data-theme="e">
 	<div data-role="header" data-position="fixed" data-add-back-btn="true" data-back-btn-text="返回">
 	<h1>我的订单</h1>
@@ -77,13 +70,13 @@ $basename = basename(__FILE__, '.php');
 		<p>商品详情:&nbsp;<span id="detail"></span></p>
 		<p>营业厅:&nbsp;<span id="office_id"></span></p>
 		<p>价格:&nbsp;￥<span id="feesum"></span></p>
-
+		
 		<hr color="#F7C708">
-		<p>用户信息</p>
+		
+		<p>用户信息</p></li>
 		<p>姓名:&nbsp;<span id="username"></span></p>
 		<p>手机号码:&nbsp;<span id="usermobile"></span></p>
 		<p>身份证:&nbsp;<span id="userid"></span></p>
-		
 	</div>
 
 	<div data-role="footer" data-position="fixed">
@@ -108,56 +101,93 @@ var imgurl = '<?php echo Yii::$app->getRequest()->baseUrl.'/../web/images/share-
 
 office_name = <?php echo \app\models\MOffice::getOfficeNameOption($user->gh_id); ?>;
 
-$(document).on("pageinit", "#myorder", function(){
 
-	function load_data2(i, n)
-	{
-		count++;
+function load_data1(i, n)
+{
+	$("#oid").html(n.oid);
+	$("#title").html(n.title);
+	$("#create_time").html(n.create_time);
+	$("#detail").html(n.detail);
+	$("#feesum").html(n.feesum/100);
+	$("#username").html(n.username);
+	$("#usermobile").html(n.usermobile);
+	$("#userid").html(n.userid);
+	$("#office_id").html(office_name[n.office_id]);
+	if(n.status == 0)
+		$("#status").html(n.statusName +"<span style='color:blue' class='qxdd_orderdetail' myOid="+n.oid+">&nbsp;&nbsp;取消订单</span>");
+	else
+		$("#status").html(n.statusName);
+}
 
-		text ="<li>\
-		<img src='"+imgurl+"'>\
-		<p>订单编号:&nbsp;<span color='color:blue'>"+n.oid+"</span></p>\
-		<p>下单时间:&nbsp;"+n.create_time+"</p>\
-		<p>商品名称:&nbsp;"+n.title+"</p>\
-		<p>价格:&nbsp;￥"+(n.feesum)/100+"</p>";
+function load_data2(i, n)
+{
+	count++;
 
-		if(n.status == 0) //wait to pay 
-			txt_mos ="<p>订单状态:&nbsp;"+n.statusName+"<span style='color:blue' class='qxdd' myOid="+n.oid+">&nbsp;&nbsp;取消订单</span></p>";
-		else
-			txt_mos ="<p>订单状态:&nbsp;"+n.statusName+"</p>";
+	text ="<li>\
+	<img src='"+imgurl+"'>\
+	<p>订单编号:&nbsp;<span color='color:blue'>"+n.oid+"</span></p>\
+	<p>下单时间:&nbsp;"+n.create_time+"</p>\
+	<p>商品名称:&nbsp;"+n.title+"</p>\
+	<p>价格:&nbsp;￥"+(n.feesum)/100+"</p>";
 
-		txt_mod = "<i class='ui-corner-all ui-btn-icon-right ui-icon-carat-r ddxq' myOid="+n.oid+"></i></li>";
+	if(n.status == 0) //wait to pay 
+		txt_mos ="<p>订单状态:&nbsp;"+n.statusName+"<span style='color:blue' class='qxdd' myOid="+n.oid+">&nbsp;&nbsp;取消订单</span></p>";
+	else
+		txt_mos ="<p>订单状态:&nbsp;"+n.statusName+"</p>";
 
-		text = text + txt_mos + txt_mod;
+	txt_mod = "<i class='ui-corner-all ui-btn-icon-right ui-icon-carat-r ddxq' myOid="+n.oid+"></i></li>";
 
-		$("#list_common_tbody").append(text).trigger('create');
-		//$("#list_common_tbody").append(text);
+	text = text + txt_mos + txt_mod;
 
-	}
+	$("#list_common_tbody").append(text).trigger('create');
+	//$("#list_common_tbody").append(text);
+}
 
-	function getMyOrderList()
-	{
-		$("#list_common_tbody").html('');
+function getMyOrderList()
+{
+	$("#list_common_tbody").html('');
+	$.ajax({
+	    url: "<?php echo Url::to(['wap/ajaxdata', 'cat'=>'myorder'], true) ; ?>",
+	    type:"GET",
+	    cache:false,
+	    dataType:'json',
+	    data: "&currentPage="+currentPage+"&size="+size+"&gh_id="+gh_id+"&openid="+openid,
+	    success: function(json_data){
+	        if(json_data)
+	        {
+	            $.each(json_data, load_data2);
+	        }
+
+			$('#list_common_tbody').listview('refresh');
+
+	        if(json_data.length < 3)
+	            currentPage =1;
+	    }
+	});
+}
+
+function getMyOrderListDetail(oid)
+{
 		$.ajax({
-		    url: "<?php echo Url::to(['wap/ajaxdata', 'cat'=>'myorder'], true) ; ?>",
-		    type:"GET",
-		    cache:false,
-		    dataType:'json',
-		    data: "&currentPage="+currentPage+"&size="+size+"&gh_id="+gh_id+"&openid="+openid,
-		    success: function(json_data){
-		        if(json_data)
-		        {
-		            $.each(json_data, load_data2);
-		        }
+	    url: "<?php echo Url::to(['wap/ajaxdata', 'cat'=>'orderview'], true) ; ?>",
+	    type:"GET",
+	    cache:false,
+	    dataType:'json',
+	    data: "&oid="+oid,
+	    success: function(json_data){
+	        if(json_data)
+	        {
+	           load_data1(0, json_data); 
+	        }
+	        //$.mobile.changePage("#orderdetail",{transition:"slide"});
+	        //$('#orderdetail_content').listview('refresh');
+	        $("#status").trigger('create');
 
-				$('#list_common_tbody').listview('refresh');
-	
-		        if(json_data.length < 3)
-		            currentPage =1;
-		    }
-		});
-	}
+	    }
+	});
+}
 
+$(document).on("pageinit", "#myorder", function(){
 
 	getMyOrderList();
 
@@ -173,7 +203,6 @@ $(document).on("pageinit", "#myorder", function(){
 			currentPage--;
 		getMyOrderList();
 	});
-
 
 	/*取消订单*/
 	$(document).on("tap",".qxdd",function(){
@@ -205,25 +234,6 @@ $(document).on("pageinit", "#myorder", function(){
 		});
 	});
 
-
-	function load_data1(i, n)
-	{
-		$("#oid").html(n.oid);
-		$("#title").html(n.title);
-		$("#create_time").html(n.create_time);
-		$("#detail").html(n.detail);
-		$("#feesum").html(n.feesum/100);
-		$("#username").html(n.username);
-		$("#usermobile").html(n.usermobile);
-		$("#userid").html(n.userid);
-		$("#office_id").html(office_name[n.office_id]);
-		if(n.status == 0)
-			$("#status").html(n.statusName +"<span style='color:blue' class='qxdd_orderdetail' myOid="+n.oid+">&nbsp;&nbsp;取消订单</span>");
-		else
-			$("#status").html(n.statusName);
-
-	}
-
 	/*订单详情*/
 	$(document).on("tap",".ddxq",function(){
 		oid = $(this).attr('myOid');
@@ -248,7 +258,9 @@ $(document).on("pageinit", "#myorder", function(){
 });
 
 
+
 $(document).on("pageinit", "#orderdetail", function(){
+
 	/*取消订单@在详情页*/
 	$(document).on("tap",".qxdd_orderdetail",function(){
 
@@ -270,10 +282,7 @@ $(document).on("pageinit", "#orderdetail", function(){
 		        {
 		
 		        }
-		        //$.mobile.changePage("#myorder",{transition:"slide"});
-		      	//getMyOrderList();
-		      	$("#status").trigger('create');
-
+		        getMyOrderListDetail(oid);
 		    }
 		});
 	});
