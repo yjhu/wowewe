1cade26a76d23afd463f4dcc38d04b622db40870
kzeng <kzeng@wosotech.com>
2014-08-26 21:57:28 +0800

    mod
    Signed-off-by: kzeng <kzeng@wosotech.com>

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
 	'class' => 'yii\db\Connection',
 	'dsn' => 'mysql:host=localhost;dbname=wx',
 	'username' => 'root',
-	'password' => 'hehbhehb429730',
+	'password' => '',
 	'charset' => 'utf8',
 	'enableSchemaCache' => true,
 	'schemaCacheDuration' => YII_DEBUG ? 10 : 3600,
