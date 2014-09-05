3ed91177a5f5643e31f9967795682a6c8d3ca011
hbhe <hbhe@wosotech.com>
2014-09-05 13:28:41 +0800

    add
    Signed-off-by: hbhe <hbhe@wosotech.com>

---
+++ views/wap/alipay_msg.php
@@ -0,0 +1,28 @@
+﻿<?php
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\ActiveForm;
+use yii\helpers\Url;
+use app\models\U;
+?>
+
+    
+<div data-role="page" id="page1" data-theme="c">
+
+    <?php echo $this->render('header1', ['menuId'=>'menu1','title' => '支付宝付款']); ?>
+ 
+    <div data-role="content">
+        <?php echo $msg; ?>
+    </div>
+
+    <div data-role="footer" data-position="fixed">
+        <h4>&copy; 襄阳联通 2014</h4>
+    </div>
+</div> <!-- page1 end -->
+
+
+<?php
+/*
+
+ 
+ */
---
+++ views/wap/alipay_submit.php
@@ -0,0 +1,17 @@
+﻿<?php
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\ActiveForm;
+use yii\helpers\Url;
+use app\models\U;
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>alipay</title>
+</head>
+</body>
+        <?php echo $msg; ?>
+</body>
+</html>
