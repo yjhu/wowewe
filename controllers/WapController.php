a5b64877a49107ed7ef38274239c7266541222c3
hbhe <hbhe@wosotech.com>
2014-09-10 19:04:21 +0800

    add
    Signed-off-by: hbhe <hbhe@wosotech.com>

--- controllers/WapController.php
+++ controllers/WapController.php
@@ -1249,11 +1249,12 @@ EOD;
 		if (\Yii::$app->request->isPost) 
 		{
 			$model->setAttributes($_POST['MOrder'], false);
-U::W($model->getAttributes());			
 			if ($model->pay_kind == MOrder::PAY_KIND_CASH) 
 			{
-				U::W('111111122');			
-				return $this->redirect(['wap/order']);		
+				//return $this->redirect(['wap/order']);	
+				$user = MUser::findOne(['gh_id'=>$gh_id, 'openid'=>$openid]);	
+				return $this->render('order', ['user'=>$user, 'gh_id'=>$gh_id, 'openid'=>$openid]);				
+				//return $this->render('orderinfo',['gh_id'=>$gh_id, 'openid'=>$openid, 'model' => $model]);				
 			}
 				
 			if ($model->pay_kind == MOrder::PAY_KIND_ALIWAP) 
