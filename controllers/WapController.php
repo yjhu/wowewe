e3d1276bef7ca2aef61d7f7538bb640b96719093
hbhe <hbhe@wosotech.com>
2014-09-12 17:59:30 +0800

    add
    Signed-off-by: hbhe <hbhe@wosotech.com>

--- controllers/WapController.php
+++ controllers/WapController.php
@@ -55,7 +55,7 @@ class WapController extends Controller
 	public function init()
 	{
 		//U::W(['init....', $_GET,$_POST, $GLOBALS]);
-		U::W(['init....', $_GET,$_POST]);
+		//U::W(['init....', $_GET,$_POST]);
 	}
 
 	public function beforeAction($action)
@@ -88,19 +88,31 @@ class WapController extends Controller
 	}
 
 	//http://127.0.0.1/wx/web/index.php?r=wap/oauth2cb&state=wap/luck:gh_1ad98f5481f3
+	//http://127.0.0.1/wx/web/index.php?r=wap/oauth2cb&state=wap/luck:gh_1ad98f5481f3:cid=11:oid=12
 	public function actionOauth2cb()
 	{
 		if (Yii::$app->wx->localTest)
 		{
-			$openid = Wechat::OPENID_TESTER1;
-			list($route, $gh_id) = explode(':', $_GET['state']);
-			Yii::$app->session['gh_id'] = $gh_id;
-			Yii::$app->session['openid'] = $openid;			
-			$user = MUser::findOne(['gh_id'=>$gh_id, 'openid'=>$openid]);
-			//if ($user !== null)
-			//	Yii::$app->user->login($user);
-			//return $this->redirect([$route, 'gh_id'=>$gh_id, 'openid'=>$openid]);
-			return $this->redirect([$route]);
+                    $openid = Wechat::OPENID_TESTER1;
+                    //list($route, $gh_id) = explode(':', $_GET['state']);
+                    $arr = explode(':', $_GET['state']);
+                    $route = $arr[0];
+                    $gh_id = $arr[1];
+                    unset($arr[0], $arr[1]);
+                    $r[] = $route;
+                    foreach($arr as $str)
+                    {
+                        list($key, $val) = explode('=', $str);
+                        $r[$key] = $val;
+                    }
+                    Yii::$app->session['gh_id'] = $gh_id;
+                    Yii::$app->session['openid'] = $openid;			
+                    $user = MUser::findOne(['gh_id'=>$gh_id, 'openid'=>$openid]);
+                    //if ($user !== null)
+                    //	Yii::$app->user->login($user);
+                    //return $this->redirect([$route, 'gh_id'=>$gh_id, 'openid'=>$openid]);
+                    //return $this->redirect([$route]);
+                    return $this->redirect($r);
 		}
 	
 		if (empty($_GET['code']))
@@ -113,7 +125,18 @@ class WapController extends Controller
 		{
 			return 'Sorry, we can not do anything for you without your authrization!';
 		}
-		list($route, $gh_id) = explode(':', $_GET['state']);
+		
+                $arr = explode(':', $_GET['state']);
+                $route = $arr[0];
+                $gh_id = $arr[1];
+                unset($arr[0], $arr[1]);
+                $r[] = $route;
+                foreach($arr as $str)
+                {
+                    list($key, $val) = explode('=', $str);
+                    $r[$key] = $val;
+                }
+
 		Yii::$app->wx->setGhId($gh_id);
 		$token = Yii::$app->wx->WxGetOauth2AccessToken($code);
 		if (!isset($token['access_token']))
@@ -132,13 +155,7 @@ class WapController extends Controller
 		}
 		Yii::$app->session['gh_id'] = $gh_id;
 		Yii::$app->session['openid'] = $openid;
-		//$user = MUser::findOne(['gh_id'=>$gh_id, 'openid'=>$openid]);
-		//if ($user !== null)
-		//	Yii::$app->user->login($user);
-		//else
-		//	U::W("not found, $openid");
-		//return $this->redirect([$route, 'gh_id'=>$gh_id, 'openid'=>$openid]);
-		return $this->redirect([$route]);
+              return $this->redirect($r);
 	}
 
 	//http://127.0.0.1/wx/web/index.php?r=wap/nativepackage
@@ -1630,6 +1647,80 @@ return $xmlStr;
 		return $this->render('testpay', ['model' => $model]);
 	}
 
+	public function actionOauth2cb()
+	{
+		if (Yii::$app->wx->localTest)
+		{
+                    $openid = Wechat::OPENID_TESTER1;
+                    //list($route, $gh_id) = explode(':', $_GET['state']);
+                    $arr = explode(':', $_GET['state']);
+                    $route = $arr[0];
+                    $gh_id = $arr[1];
+                    unset($arr[0], $arr[1]);
+                    $r[] = $route;
+                    foreach($arr as $str)
+                    {
+                        list($key, $val) = explode('=', $str);
+                        $r[$key] = $val;
+                    }
+                    Yii::$app->session['gh_id'] = $gh_id;
+                    Yii::$app->session['openid'] = $openid;			
+                    $user = MUser::findOne(['gh_id'=>$gh_id, 'openid'=>$openid]);
+                    //if ($user !== null)
+                    //	Yii::$app->user->login($user);
+                    //return $this->redirect([$route, 'gh_id'=>$gh_id, 'openid'=>$openid]);
+                    //return $this->redirect([$route]);
+                    return $this->redirect($r);
+		}
+	
+		if (empty($_GET['code']))
+		{
+			U::W([__METHOD__, $_GET, 'no code']);
+			return;
+		}		
+		$code = $_GET['code'];
+		if ($code == 'authdeny')
+		{
+			return 'Sorry, we can not do anything for you without your authrization!';
+		}
+		
+                $arr = explode(':', $_GET['state']);
+                $route = $arr[0];
+                $gh_id = $arr[1];
+                unset($arr[0], $arr[1]);
+                $r[] = $route;
+                foreach($arr as $str)
+                {
+                    list($key, $val) = explode('=', $str);
+                    $r[$key] = $val;
+                }
+
+		Yii::$app->wx->setGhId($gh_id);
+		$token = Yii::$app->wx->WxGetOauth2AccessToken($code);
+		if (!isset($token['access_token']))
+		{
+			U::W([__METHOD__, $token]);
+			return null;
+		}
+		$oauth2AccessToken = $token['access_token'];
+		$openid = $token['openid'];
+
+		if (isset($token['scope']) && $token['scope'] == 'snsapi_userinfo')
+		{
+			$oauth2UserInfo = Yii::$app->wx->WxGetOauth2UserInfo($oauth2AccessToken, $openid);
+			U::W($oauth2UserInfo);
+			Yii::$app->session->set('oauth2UserInfo', $oauth2UserInfo);
+		}
+		Yii::$app->session['gh_id'] = $gh_id;
+		Yii::$app->session['openid'] = $openid;
+		//$user = MUser::findOne(['gh_id'=>$gh_id, 'openid'=>$openid]);
+		//if ($user !== null)
+		//	Yii::$app->user->login($user);
+		//else
+		//	U::W("not found, $openid");
+		//return $this->redirect([$route, 'gh_id'=>$gh_id, 'openid'=>$openid]);
+              return $this->redirect($r);
+	}
 
         
 */	
