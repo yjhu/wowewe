16c5018c287407a649be241f07ef532db42b9f14
hbhe <hbhe@wosotech.com>
2014-09-26 13:27:06 +0800

    add
    Signed-off-by: hbhe <hbhe@wosotech.com>

--- views/site/error.php
+++ views/site/error.php
@@ -1,15 +1,20 @@
 <?php
 use yii\helpers\Html;
+use yii\helpers\Url;
 //$this->title = $name;
 ?>
 
 <div class="site-error">
 
-	<h2>出错啦！</h2>
-
-	<div class="error">
-		<?php echo $message.' '.Html::a("返回", Yii::$app->getRequest()->getReferrer()); ?>
-	</div>
+	<?php if ($code == 9000): ?>
+		<h2>请先关注襄阳联通官方微信</h2>
+		<?php echo Html::img(Url::to('images/wx-tuiguang2.jpg'), ['class'=>'img-responsive center-block']); ?>
+	<?php else: ?>
+		<h2>出错啦！</h2>
+		<div class="error">
+			<?php echo $message.' '.Html::a("返回", Yii::$app->getRequest()->getReferrer()); ?>
+		</div>
+	<?php endif; ?>
 
 </div>
 
