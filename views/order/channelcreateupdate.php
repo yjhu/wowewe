3fed75e0708e55d3b0e5b7369e4ffc9dd0f4e08a
hbhe <hbhe@wosotech.com>
2014-09-28 17:50:59 +0800

    add
    Signed-off-by: hbhe <hbhe@wosotech.com>

---
+++ views/order/channelcreateupdate.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+use app\models\MChannel;
+
+$this->title = $model->isNewRecord ? '新增' : '修改'.'渠道';
+$this->params['breadcrumbs'][] = ['label' => '渠道管理', 'url' => ['channellist']];
+$this->params['breadcrumbs'][] = $model->isNewRecord ? '新增' : '修改';
+?>
+<div class="muser-update">
+
+	<div class="muser-form">
+
+		<?php $form = ActiveForm::begin(); ?>
+
+		<?= $form->field($model, 'title')->textInput(['maxlength' => 64]) ?>
+
+		<?= $form->field($model, 'mobile')->textInput(['maxlength' => 24]) ?>
+
+		<div class="form-group">
+			<?= Html::submitButton($model->isNewRecord ? '增加' : '修改', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+		</div>
+
+		<?php ActiveForm::end(); ?>
+
+	</div>
+
+</div>
+
+<?php
+/*
+		<?= $form->field($model, 'channel_id')->textInput(['maxlength' => 10]) ?>
+
+		<?= $form->field($model, 'channel_id')->dropDownList(MOffice::getOfficeNameOptionAll(Yii::$app->user->identity->gh_id, false, false)) ?>
+
+
+		<?= $form->field($model, 'status')->dropDownList(MOrder::getOrderStatusOptionForOffice()) ?>
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+*/
---
+++ views/order/channellist.php
@@ -0,0 +1,105 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\GridView;
+
+use app\models\U;
+use app\models\MChannel;
+use app\models\MOffice;
+
+$this->title = '渠道管理';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="muser-index">
+
+	<h1><?php //echo Html::encode($this->title) ?></h1>
+
+    <?php //echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+		<?php echo Html::a('新增渠道', ['channelcreate'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+	<?php \yii\widgets\Pjax::begin([
+		'timeout' => 10000,
+	]); ?>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+		'options' => ['class' => 'table-responsive'],
+		'tableOptions' => ['class' => 'table table-striped'],        
+        'columns' => [
+			[
+				'label' => '渠道编号',
+				'attribute' => 'id',
+				'headerOptions' => array('style'=>'width:100px;'),	
+				//'filter'=> false,				
+			],
+			[
+				'label' => '渠道名称',
+				'attribute' => 'title',
+			],
+			[
+				'attribute' => 'mobile',
+			],
+			[
+				'label' => '微信号',
+				'value'=>function ($model, $key, $index, $column) { return empty($model->user->nickname) ? '[微信未绑定]' : $model->user->nickname; },
+				'filter'=> false,
+			],
+			[
+				'label' => '推广成绩',
+				'value'=>function ($model, $key, $index, $column) { 
+					return $model->score; 
+				},
+				'filter'=> false,
+			],
+			[
+				'label' => '推广二维码',
+                'format'=>'html',
+				'value'=>function ($model, $key, $index, $column) { 
+
+/*
+					return Html::a('<span class="glyphicon glyphicon-qrcode"></span>', $model->getQrImageUrl(), [
+						'title' => '查看推广二维码',
+						'target' => '_blank',
+					]);
+*/
+					return Html::img($model->getQrImageUrl(), ['width'=>'64']);
+				},
+				'filter'=> false,
+			],
+
+            [
+				'class' => 'yii\grid\ActionColumn',
+				'template' => '{channelupdate} {channeldelete}',
+				'buttons' => [
+					'channelupdate' => function ($url, $model) {
+						return Html::a('<span class="glyphicon glyphicon-pencil"></span>', $url, [
+							'title' => Yii::t('yii', 'Update'),
+							'data-pjax' => '0',
+						]);
+					},
+					'channeldelete' => function ($url, $model) {
+						return Html::a('<span class="glyphicon glyphicon-trash"></span>', $url, [
+							'title' => Yii::t('yii', 'Delete'),
+							'data-confirm' => Yii::t('yii', '确认要删除此渠道?'),
+							'data-method' => 'post',
+							'data-pjax' => '0',
+							//'data-pjax' => '1',
+						]);
+					}
+				],
+			],
+
+        ],
+    ]); ?>
+
+	<?php \yii\widgets\Pjax::end(); ?>
+</div>
+
+<?php
+/*
+
+*/
