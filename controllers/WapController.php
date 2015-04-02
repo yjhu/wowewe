7f5d536fe52d40ea5b94ec9422d4aa897f010bb1
hbhe <hbhe@wosotech.com>
2015-04-02 22:05:25 +0800

    add
    Signed-off-by: hbhe <hbhe@wosotech.com>

--- controllers/WapController.php
+++ controllers/WapController.php
@@ -2709,7 +2709,6 @@ U::W('aaaaa......'.$user_founder->mobile);
         $model->speed_up = $speed_up;
         $model->speed_down = $speed_down;
         $model->speed_delay = $speed_delay;
-        $model->media_id = $media_id;
         $model->pic_url = "{$gh_id}_{$media_id}.jpg";
         $model->save(false);        
         return json_encode(['code'=>0]);
