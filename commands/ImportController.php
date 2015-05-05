d979d12bf725ced9ab62a352c1150e26d0f92508
Hu Yajun <yjhu@wosotech.com>
2015-05-05 19:30:45 +0800

    yjhu

--- commands/ImportController.php
+++ commands/ImportController.php
@@ -1,65 +1,62 @@
 <?php
 
 /*
-yii import/supervior <filename>
-*/
+  yii import/supervior <filename>
+ */
 
 namespace app\commands;
 
 use yii;
 use yii\console\Controller;
-
 use app\models\MOffice;
 use app\models\MStaff;
 use app\models\MMarketingRegion;
 use app\models\MMarketingServiceCenter;
 use app\models\MOfficeCampaignPicCategory;
 
-class ImportController extends Controller
-{
+class ImportController extends Controller {
+
     /**
      * This command import office supervision data.
      * @param string $filename the file to be imported to DB.
      */
-    public function actionSupervisor($filename = 'supervisor.csv')
-    {
-        $file = Yii::$app->getRuntimePath().DIRECTORY_SEPARATOR.'imported_data'.DIRECTORY_SEPARATOR.$filename;
+    public function actionSupervisor($filename = 'supervisor.csv') {
+        $file = Yii::$app->getRuntimePath() . DIRECTORY_SEPARATOR . 'imported_data' . DIRECTORY_SEPARATOR . $filename;
         $fh = fopen($file, "r");
-        $i=0;
-        while (!feof($fh)) 
-        {
+        $i = 0;
+        while (!feof($fh)) {
             $line = fgets($fh);
             $i++;
             if (empty($line))
                 continue;
-            $fields = explode(",", $line);    
+            $fields = explode(",", $line);
             $office_name = trim($fields[0]);
-            $office_name_utf8 = iconv('GBK','UTF-8//IGNORE', $office_name);
+            $office_name_utf8 = iconv('GBK', 'UTF-8//IGNORE', $office_name);
             $msc_name = trim(trim($fields[1]), "0..9");
-            $msc_name_utf8 = iconv('GBK','UTF-8//IGNORE', $msc_name);
+            $msc_name_utf8 = iconv('GBK', 'UTF-8//IGNORE', $msc_name);
             $region_name = trim(trim($fields[2]), "0..9");
-            $region_name_utf8 = iconv('GBK','UTF-8//IGNORE', $region_name);
+            $region_name_utf8 = iconv('GBK', 'UTF-8//IGNORE', $region_name);
             $supervisor_name = trim($fields[3]);
-            $supervisor_name_utf8 = iconv('GBK','UTF-8//IGNORE', $supervisor_name);
+            $supervisor_name_utf8 = iconv('GBK', 'UTF-8//IGNORE', $supervisor_name);
             $supervisor_mobile = trim($fields[4]);
-            
+
             $region = MMarketingRegion::findOne(['name' => $region_name_utf8]);
             if (empty($region)) {
                 $region = new MMarketingRegion;
                 $region->name = $region_name_utf8;
                 $region->save(false);
             }
-            
+
             $msc = MMarketingServiceCenter::findOne(['name' => $msc_name_utf8]);
             if (empty($msc)) {
                 $msc = new MMarketingServiceCenter;
                 $msc->name = $msc_name_utf8;
-                $msc->region_id = $region -> id;
+                $msc->region_id = $region->id;
                 $msc->save(false);
             }
-            
+
             $office = MOffice::findOne(['title' => $office_name_utf8]);
-            if (empty($office)) {                
+            if (empty($office)) {
                 $office = new MOffice;
                 $office->gh_id = 'gh_03a74ac96138'; // 襄阳联通公共ID
                 $office->title = $office_name_utf8;
@@ -72,9 +69,9 @@ class ImportController extends Controller
                     'msc_id' => $msc->id,
                 ])->execute();
             }
-            
+
             $staff = MStaff::findOne(['name' => $supervisor_name_utf8]);
-            if (empty($staff)) {                
+            if (empty($staff)) {
                 $staff = new MStaff;
                 $staff->office_id = 25;
                 $staff->name = $supervisor_name_utf8;
@@ -89,35 +86,32 @@ class ImportController extends Controller
                     'staff_id' => $staff->staff_id,
                 ])->execute();
             }
-        
         }
-        fclose($fh);   
+        fclose($fh);
     }
-    
-        /**
+
+    /**
      * This command import office supervision data.
      * @param string $filename the file to be imported to DB.
      */
-    public function actionScorer($filename = 'scorer.csv')
-    {
-        $file = Yii::$app->getRuntimePath().DIRECTORY_SEPARATOR.'imported_data'.DIRECTORY_SEPARATOR.$filename;
+    public function actionScorer($filename = 'scorer.csv') {
+        $file = Yii::$app->getRuntimePath() . DIRECTORY_SEPARATOR . 'imported_data' . DIRECTORY_SEPARATOR . $filename;
         $fh = fopen($file, "r");
-        $i=0;
-        while (!feof($fh)) 
-        {
+        $i = 0;
+        while (!feof($fh)) {
             $line = fgets($fh);
             $i++;
             if (empty($line))
                 continue;
-            $fields = explode(",", $line);    
+            $fields = explode(",", $line);
             $scorer_name = trim($fields[0]);
-            $scorer_name_utf8 = iconv('GBK','UTF-8//IGNORE', $scorer_name);
+            $scorer_name_utf8 = iconv('GBK', 'UTF-8//IGNORE', $scorer_name);
             $scorer_department = trim(trim($fields[1]), "0..9");
-            $scorer_department_utf8 = iconv('GBK','UTF-8//IGNORE', $scorer_department);
+            $scorer_department_utf8 = iconv('GBK', 'UTF-8//IGNORE', $scorer_department);
             $scorer_position = trim(trim($fields[2]), "0..9");
-            $scorer_position_utf8 = iconv('GBK','UTF-8//IGNORE', $scorer_position);
+            $scorer_position_utf8 = iconv('GBK', 'UTF-8//IGNORE', $scorer_position);
             $scorer_mobile = trim($fields[3]);
-            
+
             $scorer = \app\models\MOfficeCampaignScorer::findOne(['name' => $scorer_name_utf8, 'mobile' => $scorer_mobile]);
             if (empty($scorer)) {
                 $scorer = new \app\models\MOfficeCampaignScorer;
@@ -126,46 +120,65 @@ class ImportController extends Controller
                 $scorer->position = $scorer_position_utf8;
                 $scorer->mobile = $scorer_mobile;
                 $scorer->save(false);
-            }        
+            }
         }
-        fclose($fh);   
+        fclose($fh);
     }
-    
-    public function actionTestdata()
-    {
-        if (YII_DEBUG == false) return;
+
+    /**
+     * This command import self-operated office data.
+     * @param string $filename the file to be imported to DB.
+     */
+    public function actionSelfOperatedOffice($filename = 'self_operated_offices.csv') {
         
-        $categories = \app\models\MOfficeCampaignPicCategory::find()->all();
-        foreach ($categories as $category) {
-            $max_score = ($category->sort_order == 6) ? 10 : 18;
-            
-            $office_campaign_detail = new \app\models\MOfficeCampaignDetail;
-            $office_campaign_detail->office_id = 15;
-            $office_campaign_detail->pic_category = $category->id;
-            $office_campaign_detail->save(false);
-            
-            $office_campaign_score = new \app\models\MOfficeCampaignScore;
-            $office_campaign_score->office_campaign_id = $office_campaign_detail->id;
-            $office_campaign_score->staff_id = 451;
-            $office_campaign_score->score = rand(0, $max_score);
-            $office_campaign_score->save(false);
-            
-            $office_campaign_score = new \app\models\MOfficeCampaignScore;
-            $office_campaign_score->office_campaign_id = $office_campaign_detail->id;
-            $office_campaign_score->staff_id = 529;
-            $office_campaign_score->score = rand(0, $max_score);
-            $office_campaign_score->save(false);
-            
-            $office_campaign_score = new \app\models\MOfficeCampaignScore;
-            $office_campaign_score->office_campaign_id = $office_campaign_detail->id;
-            $office_campaign_score->staff_id = 576;
-            $office_campaign_score->score = rand(0, $max_score);
-            $office_campaign_score->save(false);           
+        Yii::$app->db->createCommand('UPDATE wx_office SET is_selfOperated=0')->execute();
+        
+        $file = Yii::$app->getRuntimePath() . DIRECTORY_SEPARATOR . 'imported_data' . DIRECTORY_SEPARATOR . $filename;
+        $fh = fopen($file, "r");
+        $i = 0;
+        while (!feof($fh)) {
+            $line = fgets($fh);
+            $i++;
+            if (empty($line))
+                continue;
+            $fields = explode(",", $line);
+            $mr_name = trim($fields[0]);
+            $mr_name_utf8 = iconv('GBK', 'UTF-8//IGNORE', $mr_name);
+            $msc_name = trim($fields[1]);
+            $msc_name_utf8 = iconv('GBK', 'UTF-8//IGNORE', $msc_name);
+            $office_name = trim($fields[2]);
+            $office_name_utf8 = iconv('GBK', 'UTF-8//IGNORE', $office_name);
+            $director_name = trim($fields[3]);
+            $director_name_utf8 = iconv('GBK', 'UTF-8//IGNORE', $director_name);
+            $director_mobile = trim($fields[4]);
+
+            $office = \app\models\MOffice::findOne(['title' => $office_name_utf8]);
+            if (!empty($office)) {
+                $in_msc = (new \yii\db\Query())->select('*')
+                        ->from('wx_rel_office_msc')
+                        ->where(['office_id' => $office->office_id])
+                        ->count();
+                if (!$in_msc) {
+                    echo "{$office_name} does not belong to any msc." . PHP_EOL;
+                    $msc = \app\models\MMarketingServiceCenter::findOne(['name' => $msc_name_utf8]);
+                    if (!empty($msc)) {
+                        yii::$app->db->createCommand()->insert('wx_rel_office_msc', [
+                            'office_id' => $office->office_id,
+                            'msc_id' => $msc->id,
+                        ])->execute();
+                    } else {
+                        echo "can't find msc by name ({$msc_name})" . PHP_EOL;
+                    }
+                }
+                $office->manager = $director_name_utf8;
+                $office->mobile = $director_mobile;
+                $office->is_selfOperated = 1;
+                $office->save(false);
+            } else {
+                echo "can't find office by name ({$office_name})" . PHP_EOL;
+            }
         }
+        fclose($fh);
     }
-    
-    public function actionTest()
-    {
-        var_dump(\app\models\MOfficeCampaignScore::getOfficeScoreByOfficeAndPicCategory(15, 1));
-    }
+
 }
