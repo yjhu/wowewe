4bac202c7769901b835c5e808f3bec87acc69ff9
kzeng <kzeng@wosotech.com>
2015-05-06 22:01:43 +0800

    add
    Signed-off-by: kzeng <kzeng@wosotech.com>

---
+++ views/wap/officeorder.php
@@ -0,0 +1,105 @@
+<?php
+  use yii\helpers\Html;
+    use yii\helpers\Url;
+    use app\models\U;
+    use app\models\MUser;
+
+
+
+?>
+    
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>襄阳联通</title>
+
+    <!-- Sets initial viewport load and disables zooming  -->
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
+
+    <!-- Makes your prototype chrome-less once bookmarked to your phone's home screen -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+    <!-- Include the compiled Ratchet CSS -->
+    <link href="/wx/web/ratchet/dist/css/ratchet.css" rel="stylesheet">
+
+    <style type="text/css">
+      .orderitem{
+          color:#aaaaaa;
+          font-size: 11pt;
+      }
+    </style>
+  
+    <script src="http://libs.useso.com/js/jquery/2.1.1/jquery.min.js"></script>
+    <!-- Include the compiled Ratchet JS -->
+    <script src="/wx/web/ratchet/dist/js/ratchet.js"></script>
+  </head>
+  <body>
+
+    <!-- Make sure all your bars are the first things in your <body> -->
+
+    <header class="bar bar-nav">
+      <a class="icon icon-left-nav pull-left" id="btn_back" onclick="javascript:history.back()"></a>
+      <h1 class="title">
+       营业厅订单
+      </h1>
+
+    </header>
+
+    <!-- Wrap all non-bar HTML in the .content div (this is actually what scrolls) -->
+    <div class="content">
+
+       <ul class="table-view">
+   
+            <li class="table-view-cell media">
+              <a data-ignore="push" class="navigate-right" href="<?php echo  Url::to(['officeorderdetail'],true) ?>">
+
+                <img class="media-object pull-left" src="../web/images/comm-icon/upload-pic-64x64.png" width="80" height="80">
+               
+                <div class="media-body">
+                  <p><span class="orderitem">订单编号</span>&nbsp;&nbsp;1234567890</p>
+                  <p><span class="orderitem">下单时间</span>&nbsp;&nbsp;2015-05-06 18:00:00</p>
+                  <p><span class="orderitem">商品名称</span>&nbsp;&nbsp;联想A3600</p>
+                  <p><span class="orderitem">价格</span>&nbsp;&nbsp;￥399</p>
+                </div>
+              </a>
+            </li>
+
+            <li class="table-view-cell media">
+              <a data-ignore="push" class="navigate-right" href="<?php echo  Url::to(['officeorderdetail'],true) ?>">
+
+                <img class="media-object pull-left" src="../web/images/comm-icon/upload-pic-64x64.png" width="80" height="80">
+               
+                <div class="media-body">
+                  <p><span class="orderitem">订单编号</span>&nbsp;&nbsp;1234567890</p>
+                  <p><span class="orderitem">下单时间</span>&nbsp;&nbsp;2015-05-06 18:00:00</p>
+                  <p><span class="orderitem">商品名称</span>&nbsp;&nbsp;联想A3600</p>
+                  <p><span class="orderitem">价格</span>&nbsp;&nbsp;￥399</p>
+                </div>
+              </a>
+            </li>
+
+            <li class="table-view-cell media">
+              <a data-ignore="push" class="navigate-right" href="<?php echo  Url::to(['officeorderdetail'],true) ?>">
+
+                <img class="media-object pull-left" src="../web/images/comm-icon/upload-pic-64x64.png" width="80" height="80">
+               
+                <div class="media-body">
+                  <p><span class="orderitem">订单编号</span>&nbsp;&nbsp;1234567890</p>
+                  <p><span class="orderitem">下单时间</span>&nbsp;&nbsp;2015-05-06 18:00:00</p>
+                  <p><span class="orderitem">商品名称</span>&nbsp;&nbsp;联想A3600</p>
+                  <p><span class="orderitem">价格</span>&nbsp;&nbsp;￥399</p>
+                </div>
+              </a>
+            </li>                   
+        </ul>
+        &nbsp;<br>&nbsp;<br>&nbsp;<br> 
+
+    </div>
+
+
+
+  </body>
+</html>
