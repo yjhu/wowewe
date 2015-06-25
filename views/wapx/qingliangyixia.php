4ff61176449a7ffb6c3e86bd1a17ffb957fc2cfb
kzeng <kzeng@wosotech.com>
2015-06-25 18:12:22 +0800

    a
    Signed-off-by: kzeng <kzeng@wosotech.com>

---
+++ views/wapx/qingliangyixia.php
@@ -0,0 +1,71 @@
+<html>
+<head>
+<title>清凉一夏活动模板</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <!-- Sets initial viewport load and disables zooming  -->
+    <meta name="viewport" content="initial-scale=0.52, maximum-scale=1, user-scalable=yes, minimal-ui">
+
+    <!-- Makes your prototype chrome-less once bookmarked to your phone's home screen -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+    <!-- Include the compiled Ratchet CSS -->
+    <link href="/wx/web/ratchet/dist/css/ratchet.css" rel="stylesheet">
+    <link rel="stylesheet" href="http://libs.useso.com/js/font-awesome/4.2.0/css/font-awesome.min.css">
+    <style type="text/css">
+
+
+    </style>
+
+    <script src="http://libs.useso.com/js/jquery/2.1.1/jquery.min.js"></script>
+    <!-- Include the compiled Ratchet JS -->
+    <script src="/wx/web/ratchet/dist/js/ratchet.js"></script>
+</head>
+<body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<!-- Save for Web Slices (清凉一夏活动模板.psd) -->
+<table id="__01" width="100%" height="1136" border="0" cellpadding="0" cellspacing="0">
+	<tr>
+		<td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_01.jpg"  alt=""></td>
+		<td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_02.jpg"  alt=""></td>
+		<td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_03.jpg"  alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_04.jpg"  alt=""></td>
+		<td>
+			<a href="javascript:alert('抢个礼盒!')">
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_05.jpg"  alt=""></a></td>
+		<td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_06.jpg"  alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_07.jpg"  alt=""></td>
+		<td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_08.jpg"  alt=""></td>
+		<td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_09.jpg"  alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_10.jpg"  alt=""></td>
+		<td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_11.jpg" alt=""></td>
+		<td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_12.jpg"  alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_13.jpg"  alt=""></td>
+		<td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_14.jpg"  alt=""></td>
+		<td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_15.jpg"  alt=""></td>
+	</tr>
+</table>
+<!-- End Save for Web Slices -->
+</body>
+</html>
