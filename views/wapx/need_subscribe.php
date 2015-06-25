33ead58ed4008be7ed3833a9073950c5d4941e77
kzeng <kzeng@wosotech.com>
2015-06-25 20:11:10 +0800

    a
    Signed-off-by: kzeng <kzeng@wosotech.com>

--- views/wapx/need_subscribe.php
+++ views/wapx/need_subscribe.php
@@ -1,12 +1,47 @@
-﻿<?php
-use yii\helpers\Html;
-use yii\helpers\Url;
-//$this->title = $name;
-?>
+﻿<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>请先关注襄阳联通官方微信</title>
 
-<div class="row site-error">
-    <div class="jumbotron">
-			<h2>请先关注襄阳联通官方微信</h2>
-			<?php echo Html::img(Url::to('images/wx-tuiguang2.jpg'), ['class'=>'img-responsive center-block']); ?>
-    </div>
-</div>
+        <!-- Sets initial viewport load and disables zooming  -->
+        <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
+
+        <!-- Makes your prototype chrome-less once bookmarked to your phone's home screen -->
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+        <!-- Include the compiled Ratchet CSS -->
+        <link href="./ratchet/dist/css/ratchet.css" rel="stylesheet">
+        <link rel="stylesheet" href="http://libs.useso.com/js/font-awesome/4.2.0/css/font-awesome.min.css">
+
+        <link href="./php-emoji/emoji.css" rel="stylesheet">    
+    </head>
+    <body>
+
+
+		<div class="content">
+			<br>
+			<ol>
+				<li>
+				长按下图，选择‘识别图中二维码’
+				</li>
+				<li>
+				然后猛戳‘关注’，即可关注襄阳联通
+				</li>
+			</ol>
+
+     		<img width=100% src="/wx/web/images/guanzhu.jpg?v1">
+
+
+			<nav class="bar bar-tab">
+				<a class="tab-item" href="#">
+				襄阳联通&copy;2015
+				</a>
+			</nav>
+		</div>
+
+   
+    </body>
+</html>
+
--- views/wapx/qingliangyixia.php
+++ views/wapx/qingliangyixia.php
@@ -1,17 +1,11 @@
 <html>
 <head>
-<title>清凉一夏活动模板</title>
+<title>清凉一夏活动</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <!-- Sets initial viewport load and disables zooming  -->
     <meta name="viewport" content="initial-scale=0.52, maximum-scale=1, user-scalable=yes, minimal-ui">
 
-    <!-- Makes your prototype chrome-less once bookmarked to your phone's home screen -->
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
 
-    <!-- Include the compiled Ratchet CSS -->
-    <link href="/wx/web/ratchet/dist/css/ratchet.css" rel="stylesheet">
-    <link rel="stylesheet" href="http://libs.useso.com/js/font-awesome/4.2.0/css/font-awesome.min.css">
     <style type="text/css">
 
 
@@ -26,44 +20,45 @@
 <table id="__01" width="100%" height="1136" border="0" cellpadding="0" cellspacing="0">
 	<tr>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_01.jpg"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_01.jpg?v1"  alt=""></td>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_02.jpg"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_02.jpg?v1"  alt=""></td>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_03.jpg"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_03.jpg?v1"  alt=""></td>
 	</tr>
 	<tr>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_04.jpg"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_04.jpg?v1"  alt=""></td>
 		<td>
 			<a href="javascript:alert('抢个礼盒!')">
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_05.jpg"  alt=""></a></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_05.jpg?v1"  alt=""></a></td>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_06.jpg"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_06.jpg?v1"  alt=""></td>
 	</tr>
 	<tr>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_07.jpg"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_07.jpg?v1"  alt=""></td>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_08.jpg"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_08.jpg?v1"  alt=""></td>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_09.jpg"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_09.jpg?v1"  alt=""></td>
 	</tr>
 	<tr>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_10.jpg"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_10.jpg?v1"  alt=""></td>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_11.jpg" alt=""></td>
+			<a href="javascript:alert('去抽奖!')">
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_11.jpg?v1" alt=""></a></td>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_12.jpg"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_12.jpg?v1"  alt=""></td>
 	</tr>
 	<tr>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_13.jpg"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_13.jpg?v1"  alt=""></td>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_14.jpg"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_14.jpg?v1"  alt=""></td>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_15.jpg"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_15.jpg?v1"  alt=""></td>
 	</tr>
 </table>
 <!-- End Save for Web Slices -->
