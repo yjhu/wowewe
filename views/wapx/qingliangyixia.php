7ee2175519e145fba449ea09e34428323cdacd62
kzeng <kzeng@wosotech.com>
2015-06-26 18:33:16 +0800

    a

--- views/wapx/qingliangyixia.php
+++ views/wapx/qingliangyixia.php
@@ -1,9 +1,9 @@
 <html>
 <head>
-<title>清凉一夏活动</title>
+<title>缤纷盛夏活动</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <!-- Sets initial viewport load and disables zooming  -->
-    <meta name="viewport" content="initial-scale=0.52, maximum-scale=1, user-scalable=yes, minimal-ui">
+    <meta name="viewport" content="initial-scale=0.6, maximum-scale=1, user-scalable=yes, minimal-ui">
 
 
     <style type="text/css">
@@ -17,48 +17,49 @@
 </head>
 <body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
 <!-- Save for Web Slices (清凉一夏活动模板.psd) -->
+
 <table id="__01" width="100%" height="1136" border="0" cellpadding="0" cellspacing="0">
 	<tr>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_01.jpg?v1"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_01.jpg?v3"  alt=""></td>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_02.jpg?v1"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_02.jpg?v3"  alt=""></td>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_03.jpg?v1"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_03.jpg?v3"  alt=""></td>
 	</tr>
 	<tr>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_04.jpg?v1"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_04.jpg?v3"  alt=""></td>
 		<td>
 			<a href="javascript:alert('抢个礼盒!')">
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_05.jpg?v1"  alt=""></a></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_05.jpg?v3"  alt=""></a></td>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_06.jpg?v1"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_06.jpg?v3"  alt=""></td>
 	</tr>
 	<tr>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_07.jpg?v1"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_07.jpg?v3"  alt=""></td>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_08.jpg?v1"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_08.jpg?v3"  alt=""></td>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_09.jpg?v1"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_09.jpg?v3"  alt=""></td>
 	</tr>
 	<tr>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_10.jpg?v1"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_10.jpg?v3"  alt=""></td>
 		<td>
 			<a href="javascript:alert('去抽奖!')">
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_11.jpg?v1" alt=""></a></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_11.jpg?v3" alt=""></a></td>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_12.jpg?v1"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_12.jpg?v3"  alt=""></td>
 	</tr>
 	<tr>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_13.jpg?v1"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_13.jpg?v3"  alt=""></td>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_14.jpg?v1"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_14.jpg?v3"  alt=""></td>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_15.jpg?v1"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_15.jpg?v3"  alt=""></td>
 	</tr>
 </table>
 <!-- End Save for Web Slices -->
