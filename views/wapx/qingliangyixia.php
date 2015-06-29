8f52e5ac1863d521548255e175ef0c59bc608ee1
kzeng <kzeng@wosotech.com>
2015-06-29 12:44:50 +0800

    a
    Signed-off-by: kzeng <kzeng@wosotech.com>

--- views/wapx/qingliangyixia.php
+++ views/wapx/qingliangyixia.php
@@ -1,3 +1,9 @@
+<?php
+	use yii\helpers\Html;
+    use yii\helpers\Url;
+
+    use app\models\U;
+?>
 <html>
 <head>
 <title>缤纷盛夏活动</title>
@@ -21,47 +27,67 @@
 <table id="__01" width="100%" height="1136" border="0" cellpadding="0" cellspacing="0">
 	<tr>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_01.jpg?v5"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_01.jpg?v8"  alt=""></td>
+		<td><a href="javascript:alert('确认已关注!')">
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_02.jpg?v8"  alt=""></a></td>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_02.jpg?v5"  alt=""></td>
-		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_03.jpg?v5"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_03.jpg?v8"  alt=""></td>
 	</tr>
 	<tr>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_04.jpg?v5"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_04.jpg?v8"  alt=""></td>
 		<td>
-			<a href="javascript:alert('抢个礼盒!')">
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_05.jpg?v5"  alt=""></a></td>
+		
+			<a href="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx1b122a21f985ea18&redirect_uri=http%3A%2F%2Fwosotech.com%2Fwx%2Fweb%2Findex.php%3Fr%3Dwap%2Foauth2cb&response_type=code&scope=snsapi_base&state=wapx/yaoyiyao:gh_03a74ac96138#wechat_redirect">
+		
+			<!--
+			<a href="javascript:chailihe();">
+			-->
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_05.jpg?v8"  alt="">
+			</a>
+		</td>
+
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_06.jpg?v5"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_06.jpg?v8"  alt=""></td>
 	</tr>
 	<tr>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_07.jpg?v5"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_07.jpg?v8"  alt=""></td>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_08.jpg?v5"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_08.jpg?v8"  alt=""></td>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_09.jpg?v5"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_09.jpg?v8"  alt=""></td>
 	</tr>
 	<tr>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_10.jpg?v5"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_10.jpg?v8"  alt=""></td>
 		<td>
-			<a href="javascript:alert('去抽奖!')">
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_11.jpg?v5" alt=""></a></td>
+			<a href="javascript:alert('月底才有抽奖机会哟!')">
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_11.jpg?v8" alt=""></a></td>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_12.jpg?v5"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_12.jpg?v8"  alt=""></td>
 	</tr>
 	<tr>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_13.jpg?v5"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_13.jpg?v8"  alt=""></td>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_14.jpg?v5"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_14.jpg?v8"  alt=""></td>
 		<td>
-			<img src="\wx\web\images\qingliangyixia\qingliangyixia_15.jpg?v5"  alt=""></td>
+			<img src="\wx\web\images\qingliangyixia\qingliangyixia_15.jpg?v8"  alt=""></td>
 	</tr>
 </table>
 <!-- End Save for Web Slices -->
+
+
+<script type="text/javascript">
+	function chailihe()
+	{
+		alert('chailihe');
+
+		url ='https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx1b122a21f985ea18&redirect_uri=http%3A%2F%2Fwosotech.com%2Fwx%2Fweb%2Findex.php%3Fr%3Dwap%2Foauth2cb&response_type=code&scope=snsapi_base&state=wapx/yaoyiyao:gh_03a74ac96138#wechat_redirect';
+
+		location.href=url;
+	}
+</script>
 </body>
 </html>
